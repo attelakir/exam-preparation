29db57da1ede4b59760d7dc04d30789ed3581465
attelakir <71684236+attelakir@users.noreply.github.com>
2022-01-11 08:39:02 +0300

    fixed implementation

--- smartPointers/scoped_ptr.hpp
+++ smartPointers/scoped_ptr.hpp
@@ -1,45 +1,50 @@
 #ifndef EXAM_PREPARATION_SCOPED_PTR_HPP
 #define EXAM_PREPARATION_SCOPED_PTR_HPP
 
-template <class T>
+#include <memory>
+
+template<class T>
 class scoped_ptr {
 public:
-    explicit scoped_ptr(T*);
+    explicit scoped_ptr(T *value = nullptr);
+
     ~scoped_ptr();
-    T* operator->();
-    T& operator*();
-    T* get();
+
+    T *operator->();
+
+    T &operator*();
+
+    T *get() const;
+
 private:
-    T* ptr = nullptr;
+    T *ptr;
 };
 
-template <class T>
-scoped_ptr<T>::scoped_ptr(T* value) {
-    ptr = value;
-}
+template<class T>
+scoped_ptr<T>::scoped_ptr(T *value) : ptr(value) {}
 
-template <class T>
+template<class T>
 scoped_ptr<T>::~scoped_ptr() {
     delete ptr;
 }
 
-template <class T>
-T* scoped_ptr<T>::operator->() {
+template<class T>
+T *scoped_ptr<T>::operator->() {
     return ptr;
 }
 
-template <class T>
-T& scoped_ptr<T>::operator*() {
+template<class T>
+T &scoped_ptr<T>::operator*() {
     return *ptr;
 }
 
-template <class T>
-T* scoped_ptr<T>::get() {
+template<class T>
+T *scoped_ptr<T>::get() const {
     return ptr;
 }
 
 template<typename T, typename... Args>
-scoped_ptr<T> make_scoped(Args&&... args) {
+scoped_ptr<T> make_scoped(Args &&... args) {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
